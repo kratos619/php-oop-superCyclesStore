16f568da474ba26a2baf853ffa3a02998784e838
kratos619 <gaurav.palaspagar@hotmail.com>
2018-06-13 15:59:07 +0530

    Initial commit

---
+++ private/db_credentials.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gaurav
+ * Date: 13-06-2018
+ * Time: 13:34
+ */
+define("DB_SERVER","localhost");
+define("DB_USER","root");
+define("DB_PASS",null);
+define("DB_NAME","chain_gang");
+
+?>
+
---
+++ private/db_functions.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gaurav
+ * Date: 13-06-2018
+ * Time: 15:48
+ */
+
+function db_connect(){
+
+    $connection = new mysqli(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+    confirm_db_connect($connection);
+    return $connection;
+}
+
+function confirm_db_connect($connection){
+if($connection->connect_errno){
+    $msg = "db failed";
+    $msg .= $connection->connect_errno;
+    $msg .= " (" . $connection->connect_errno . ") ";
+    exit($msg);
+}
+
+}
+
+?>
